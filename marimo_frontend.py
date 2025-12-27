3cc5dd3a4acd3bddf11f093188d20fd7719f871b
hsa <heming.aldrin@proton.me>
2025-12-27 11:08:25 +0100

    try out marimo for this project

---
+++ marimo_frontend.py
@@ -0,0 +1,33 @@
+import marimo
+
+__generated_with = "0.18.4"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return
+
+
+@app.cell
+def _():
+    import polars as pl
+    import polars.selectors as cs
+    from millify import millify
+
+    from utils import (
+        combined_property_and_stocks,
+    )
+
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
